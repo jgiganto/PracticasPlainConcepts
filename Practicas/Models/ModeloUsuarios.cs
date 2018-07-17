e02ba40704a9ad96c6203ff031dabe6e2366b5e6
Jesús Giganto Díez <jgiganto@plainconcepts.com>
2018-07-17 09:11:36 +0200

    Funciona Dapper listar e insertar

--- Practicas/Models/ModeloUsuarios.cs
+++ Practicas/Models/ModeloUsuarios.cs
@@ -10,9 +10,15 @@ namespace Practicas.Models
     {
         public List<NewUserModel> GetUsuarios()
         {
-            var query = @"SELECT * FROM Users";
+            var query = @"SELECT Nombre FROM Users";             
             var listausuarios = Exec<NewUserModel>(query);
-            return (List<NewUserModel>)listausuarios;
-        }       
+            return (List<NewUserModel>)listausuarios;                         
+        } 
+        
+        public void InsertarUsuario(String nombre)
+        {
+            var query = @"INSERT INTO Users (Nombre)  Values ('" + nombre +  "')";
+            var listausuarios = Exec<NewUserModel>(query);
+        }
     }   
  }
--- Practicas/Models/NewUserModel.cs
+++ Practicas/Models/NewUserModel.cs
@@ -6,7 +6,7 @@ namespace Practicas.Models
     public class NewUserModel
     {
         [Required(ErrorMessageResourceName = nameof(CommonResources.UserIsRequired),ErrorMessageResourceType = typeof(CommonResources))]
-        public string Name { get; set; }
+        public string Nombre { get; set; }
         
     }
 }
