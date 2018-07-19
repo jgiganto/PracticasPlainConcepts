3db45e3c5d105ff8bbea1461fd0d9911c64ec6bd
Jesús Giganto Díez <jgiganto@plainconcepts.com>
2018-07-19 08:16:25 +0200

    asd

--- Practicas/Controllers/UsersController.cs
+++ Practicas/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
 
@@ -32,18 +33,29 @@ namespace Practicas.Controllers
         [HttpPost]
         public ActionResult Index(String camponombre, String campopw)
         {
+            int count = 0;
             this.CrearModelo();
             NewUserModel usuario = new NewUserModel();
             usuario.Nickname = camponombre;
             usuario.Password = campopw;
             IEnumerable<NewUserModel> result =  modelo.VerificarUsuario(usuario);
-            int num = Enumerable.Cast<int>(result).ToString().Count(); 
-
-            if ( num != 1  )
+            usuario = result.FirstOrDefault();
+            foreach (var u in result)
+            {
+                count++;
+            }
+             
+            if (count == 1 )
             {
                 ViewBag.res = "Usuario correcto";
                 ViewBag.color = "green";
+                FormsAuthenticationTicket ticket =
+                    new FormsAuthenticationTicket(1, usuario.Name.ToString(), DateTime.Now, DateTime.Now.AddMinutes(15), true, usuario.UserId.ToString(), FormsAuthentication.FormsCookiePath);
+                String ticketEncrypted = FormsAuthentication.Encrypt(ticket);
+                HttpCookie httpCookie = new HttpCookie("cookiecliente", ticketEncrypted);
+                Response.Cookies.Add(httpCookie);
                
+
             }
             else
             {
